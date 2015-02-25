2a3909052b64a5aa37f8fdb8776d2aef14cacbce
Kristopher Cost <me@krisco.st>
2015-02-24 17:20:45 -0800

    moving some things around

--- streaming.go
+++ streaming.go
@@ -47,7 +47,7 @@ func New(channelName string, variant string, status chan string) (*Watcher, erro
 
 type PlaylistWatcher struct {
 	ChannelName string
-	Variant string
+	Variant *m3u8.Variant
 
 	Token       string
 	Signature   string
@@ -58,12 +58,17 @@ type PlaylistWatcher struct {
 func NewPlaylistWatcher(channelName, variant string, status chan string) (*PlaylistWatcher, error) {
 	p := &PlaylistWatcher{
 		ChannelName: channelName,
-		Variant: variant,
 		Output:      make(chan string, 1024),
 		Status: status,
 	}
 
 	err := p.getToken()
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = p.getVariant(variant)
 
 	if err != nil {
 		return nil, err
@@ -102,46 +107,64 @@ func (p *PlaylistWatcher) getToken() error {
 	return nil
 }
 
+func (p *PlaylistWatcher) getVariant(desired_variant string) error {
+	base_url, _ := url.Parse(fmt.Sprintf(USHER_API_MASK, p.ChannelName, p.Token, p.Signature, 123456))
+	req, _ := http.NewRequest("GET", base_url.String(), nil)
+
+	resp, err := http.DefaultClient.Do(req)
+
+	if err != nil {
+		return err
+	}
+
+	if resp != nil && resp.StatusCode != 200 {
+		return fmt.Errorf("Got a response from USHER: %s", resp.Status)
+	}
+
+	playlist, _, _ := m3u8.DecodeFrom(resp.Body, true)
+	master_playlist := playlist.(*m3u8.MasterPlaylist)
+	resp.Body.Close()
+
+	variants := make([]string, len(master_playlist.Variants))
+	var target_variant *m3u8.Variant
+
+	for i, variant := range master_playlist.Variants {
+		if variant != nil {
+			variants[i] = variant.Video
+
+			if desired_variant == variant.Video {
+				target_variant = variant
+			}
+		}
+	}
+
+	log.Printf("Variants found: %s", strings.Join(variants, " "))
+
+	if target_variant == nil {
+		return fmt.Errorf("Variant specified not found for this stream.")
+	}
+
+	log.Printf("Viewing %s variant.", target_variant.Video)
+
+	p.Variant = target_variant
+	return nil
+}
+
 func (p *PlaylistWatcher) run() {
 	go func() {
 		cache := lru.New(1024)
-		base_url, _ := url.Parse(fmt.Sprintf(USHER_API_MASK, p.ChannelName, p.Token, p.Signature, 123456))
+		variant_url, _ := url.Parse(p.Variant.URI)
 
-		req, _ := http.NewRequest("GET", base_url.String(), nil)
-
+		var req *http.Request
 		var resp *http.Response
 		var err error
-
-		for resp, err = http.DefaultClient.Do(req); err != nil || (resp != nil && resp.StatusCode != 200); resp, err = http.DefaultClient.Do(req) {
-			log.Printf("Got a response from USHER: %s", resp.Status)
-			time.Sleep(5 * time.Second)
-		}
-
-		playlist, _, _ := m3u8.DecodeFrom(resp.Body, true)
-		master_playlist := playlist.(*m3u8.MasterPlaylist)
-		resp.Body.Close()
-
-		variants := make([]string, len(master_playlist.Variants))
-		var target_variant *m3u8.Variant
-
-		for i, variant := range master_playlist.Variants {
-		if variant != nil {
-			log.Printf("Variant: %#v", variant)
-				variants[i] = variant.Video
-
-				if p.Variant == variant.Video {
-					target_variant = variant
-				}
-			}
-		}
-
-		log.Printf("Variants found: %s", strings.Join(variants, " "))
-		log.Printf("Viewing %s variant.", target_variant.Video)
-		variant_base, _ := url.Parse(target_variant.URI)
+		var dir string
+		var playlist m3u8.Playlist
+		var media_playlist *m3u8.MediaPlaylist
 
 		for {
-			req, _ = http.NewRequest("GET", target_variant.URI, nil)
-			resp, err := http.DefaultClient.Do(req)
+			req, _ = http.NewRequest("GET", p.Variant.URI, nil)
+			resp, err = http.DefaultClient.Do(req)
 
 			if err != nil || (resp != nil && resp.StatusCode != 200) {
 				log.Printf("Got a response from VARIANT: %s", resp.Status)
@@ -149,10 +172,10 @@ func (p *PlaylistWatcher) run() {
 				continue
 			}
 
-			dir := path.Dir(variant_base.Path)
+			dir = path.Dir(variant_url.Path)
 
 			playlist, _, _ = m3u8.DecodeFrom(resp.Body, true)
-			media_playlist := playlist.(*m3u8.MediaPlaylist)
+			media_playlist = playlist.(*m3u8.MediaPlaylist)
 
 			resp.Body.Close()
 
@@ -161,7 +184,7 @@ func (p *PlaylistWatcher) run() {
 					_, hit := cache.Get(segment.URI)
 
 					if !hit {
-						p.Output <- fmt.Sprintf("%s://%s%s/%s", variant_base.Scheme, variant_base.Host, dir, segment.URI)
+						p.Output <- fmt.Sprintf("%s://%s%s/%s", variant_url.Scheme, variant_url.Host, dir, segment.URI)
 						cache.Add(segment.URI, nil)
 
 						if p.Status != nil {
